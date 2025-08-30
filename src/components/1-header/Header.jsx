612c19ffc24ed45e06b4a077d8cccb559775b654
Amr-Safwat <amrsafwat02@gmail.com>
2025-08-30 13:54:33 +0300

    completed menu links

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -2,6 +2,9 @@ import { useRef } from 'react';
 import '../1-header/header.css';
 
 export default function Header() {
+
+  const menu = useRef();
+
   function darkMode() {
     if (document.body.className === 'dark') {
       document.body.className = '';
@@ -9,6 +12,15 @@ export default function Header() {
       document.body.className = 'dark';
     }
   }
+
+  function showMenu() {
+    if (menu.current.className === 'links') {
+      menu.current.className = 'links fill-hei';
+    } else {
+      menu.current.className = 'links';
+    }
+  }
+
   return (
     <header>
       <div class="container">
@@ -16,12 +28,12 @@ export default function Header() {
           Portfolio
         </a>
         <div>
-          <div class="menu">
+          <div class="menu" onClick={showMenu}>
             <span></span>
             <span></span>
             <span></span>
           </div>
-          <div class="links">
+          <div class="links" ref={menu}>
             <a href="#about">About</a>
             <a href="#services">Services</a>
             <a href="#skills">Skills</a>
