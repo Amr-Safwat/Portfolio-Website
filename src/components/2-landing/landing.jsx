8ae0fa564dc75003591570f171b28f0de60a28f7
Amr-Safwat <amrsafwat02@gmail.com>
2025-09-03 10:00:10 +0300

    add some edits on landing

--- src/components/2-landing/landing.jsx
+++ src/components/2-landing/landing.jsx
@@ -1,35 +1,43 @@
 import "./landing.css";
+import imageProg from '../../assets/image-programming.png';
 
 export default function Landing() {
   return (
     <>
       <div className="landing">
         <div className="container">
-          <div className="hello">Hello, It's Me</div>
-          <div className="name">Amr Safwat</div>
-          <div className="jop">
-            And I'm a <span>Front-End Developer</span>
+          <div className="left-section">
+            <div className="hello">Hello, It's Me</div>
+            <div className="name">Amr Safwat</div>
+            <div className="jop">
+              And I'm a <span>Front-End Developer</span>
+            </div>
+            <p>
+              I'm a front end developer have some experience in building
+              responsive websites using HTML, CSS, Bootstrap, JavaScript, and
+              React.js...
+            </p>
+            <a href="../../assets/Amr-Safwat-CV.pdf" className="btn" download>
+              Download CV
+            </a>
+            <div className="icons">
+              <a href="#" className="icon">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="#" className="icon">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="#" className="icon">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="#" className="icon">
+                <i class="fab fa-facebook"></i>
+              </a>
+            </div>{' '}
           </div>
-          <p>
-            I'm a front end developer have some experience in building
-            responsive websites using HTML, CSS, Bootstrap, JavaScript, and
-            React.js...
-          </p>
-          <a href="../../assets/Amr-Safwat-CV.pdf" className="btn" download>Download CV</a>
-          <div className="icons">
-            <a href="#" className="icon">
-              <i class="fab fa-linkedin"></i>
-            </a>
-            <a href="#" className="icon">
-              <i class="fab fa-github"></i>
-            </a>
-            <a href="#" className="icon">
-              <i class="fab fa-instagram"></i>
-            </a>
-            <a href="#" className="icon">
-              <i class="fab fa-facebook"></i>
-            </a>
-          </div>          
+          <div className="right-section">
+            <img src={imageProg} alt="" />
+          </div>
         </div>
       </div>
     </>
